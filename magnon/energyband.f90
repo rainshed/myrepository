68fe3ea81113944320f25916e855b910fb52e0f2
Yupeng Wang <wangyp1@shanghaitech.edu.cn>
2019-04-06 17:11:11 +0800

    -imagine

--- magnon/energyband.f90
+++ magnon/energyband.f90
@@ -18,34 +18,61 @@ module Hamitonian
 		complex(8) :: Ham_n(12,12) 
 		integer :: i,j
 		Ham_n = 0
-		Ham_n(1,9) = J1*exp(-ima*(ka+kb))
+!		Ham_n(1,9) = J1*exp(-ima*(ka+kb))
+!		Ham_n(1,10) = J1
+!		Ham_n(1,11) = J1*exp(ima*kc)
+!		Ham_n(1,12) = J1
+!		Ham_n(2,9) = J1
+!		Ham_n(2,10) = J1
+!		Ham_n(2,11) = J1
+!		Ham_n(2,12) = J1*exp(ima*ka) 
+!		Ham_n(3,7) = J1*exp(-ima*(ka+kb))
+!		Ham_n(3,8) = J1
+!		Ham_n(3,11) = J1
+!		Ham_n(3,12) = J1
+!		Ham_n(4,7) = J1
+!		Ham_n(4,8) = J1
+!		Ham_n(4,11) = J1*exp(ima*kb)
+!		Ham_n(4,12) = J1*exp(-ima*kc)
+!		Ham_n(5,7) = J1*(ima*kc)
+!		Ham_n(5,8) = J1
+!		Ham_n(5,9) = J1
+!		Ham_n(5,10) = J1*(ima*kb)
+!		Ham_n(6,7) = J1
+!		Ham_n(6,8) = J1*(ima*ka)
+!		Ham_n(6,9) = J1
+!		Ham_n(6,10) = J1*(-ima*kc)
+!		
+!		do i = 7,12
+!			do j = 1,6
+!			Ham_n(i,j) = conjg(Ham_n(j,i))
+!			end do
+!		end do
+		Ham_n(1,9) = J1*complex(cos(-(ka+kb)),sin(-(ka+kb)))!J1*exp(-ima*(ka+kb))
 		Ham_n(1,10) = J1
-		Ham_n(1,11) = J1*exp(ima*kc)
+		Ham_n(1,11) = J1*complex(cos(kc),sin(kc))!exp(ima*kc)
 		Ham_n(1,12) = J1
 		Ham_n(2,9) = J1
 		Ham_n(2,10) = J1
 		Ham_n(2,11) = J1
-		Ham_n(2,12) = J1*exp(ima*ka) 
-		Ham_n(3,7) = J1*exp(-ima*(ka+kb))
-		Ham_n(3,8) = J1
+		Ham_n(2,12) = J1*complex(cos(ka),sin(ka))!exp(ima*ka)
 		Ham_n(3,11) = J1
 		Ham_n(3,12) = J1
-		Ham_n(4,7) = J1
-		Ham_n(4,8) = J1
-		Ham_n(4,11) = J1*exp(ima*kb)
-		Ham_n(4,12) = J1*exp(-ima*kc)
-		Ham_n(5,7) = J1*(ima*kc)
-		Ham_n(5,9) = J1
-		Ham_n(5,10) = J1*(ima*kb)
-		Ham_n(6,7) = J1
-		Ham_n(6,8) = J1*(ima*ka)
-		Ham_n(6,10) = J1*(-ima*kc)
-		
-		do i = 7,12
-			do j = 1,6
-			Ham_n(i,j) = conjg(Ham_n(j,i))
+		Ham_n(4,11) = J1*complex(cos(kb),sin(kb))!exp(ima*kb)
+		Ham_n(4,12) = J1*complex(cos(-kc),sin(-kc))!exp(-ima*kc)
+
+		do i = 1,5
+			do j = i+7,12
+				Ham_n(j-6,i+6) = Ham_n(i,j)
 			end do
 		end do
+
+		do i = 1,6
+			do j = 7,12
+				Ham_n(j,i) = conjg(Ham_n(i,j))
+			end do
+		end do
+		
 	end function
 	!--------------------------------------------------------------------------
 
@@ -57,40 +84,60 @@ module Hamitonian
 		complex(8) :: Ham_nn(12,12)
 		integer :: i,j
 		Ham_nn = 0
-		Ham_nn(1,3) = J2*exp(ima*kc)
-		Ham_nn(1,4) = J2*exp(ima*kc)
-		Ham_nn(1,5) = J2*exp(-ima*(ka+kb))
-		Ham_nn(1,6) = J2*exp(ima*kc)
-		Ham_nn(2,3) = J2*exp(ima*ka)
-		Ham_nn(2,4) = J2*exp(-ima*kb)
-		Ham_nn(2,5) = J2*exp(-ima*kb)
+		Ham_nn(1,3) = J2*complex(cos(kc),sin(kc))!exp(ima*kc)
+		Ham_nn(1,4) = J2*complex(cos(kc),sin(kc))!exp(ima*kc)
+		Ham_nn(1,5) = J2*complex(cos(-1d0*(ka+kb)),sin(-1d0*(ka+kb)))!exp(-ima*(ka+kb))
+		Ham_nn(1,6) = J2*complex(cos(kc),sin(kc))!exp(ima*kc)
+		Ham_nn(2,3) = J2*complex(cos(ka),sin(ka))!exp(ima*ka)
+		Ham_nn(2,4) = J2*complex(cos(-kb),sin(-kb))!exp(-ima*kb)
+		Ham_nn(2,5) = J2*complex(cos(-kb),sin(-kb))!exp(-ima*kb)
 		Ham_nn(2,6) = J2
-		Ham_nn(3,5) = J2*exp(-ima*(ka+kb+kc))
-		Ham_nn(3,6) = J2*exp(-ima*ka)
+		Ham_nn(3,5) = J2*complex(cos(-(ka+kb+kc)),sin(-(ka+kb+kc)))!exp(-ima*(ka+kb+kc))
+		Ham_nn(3,6) = J2*complex(cos(ka),sin(ka))!exp(-ima*ka)
 		Ham_nn(4,5) = J2
 		Ham_nn(4,6) = J2
 
-		do i = 1,6
-			do j = 1,6
+		do i = 1,5
+			do j = i+1,6,1
+				Ham_nn(j,i) = conjg(Ham_nn(i,j))
 				Ham_nn(i+6,j+6) = conjg(Ham_nn(i,j))
+				Ham_nn(j+6,i+6) = conjg(Ham_nn(i+6,j+6))
 			end do
 		end do
 
-		!----Hnn is hermite---------------------
-		do i = 1,12
-			do j = i+1,12
-				Ham_nn(j,i) = conjg(Ham_nn(i,j))
-			end do
-		end do
+!		do i = 1,6
+!			do j = 1,6
+!				Ham_nn(i+6,j+6) = conjg(Ham_nn(i,j))
+!			end do
+!		end do
+!
+!		!----Hnn is hermite---------------------
+!		do i = 1,12
+!			do j = i+1,12
+!				Ham_nn(j,i) = conjg(Ham_nn(i,j))
+!			end do
+!		end do
+
 
 	end function
 	!-------------------------------------------------------
 
 	!----get the whole Hamitonian-------------------
 	function Ham(ka,kb,kc) result(H)
+		implicit none
 		real(8) :: ka,kb,kc
-		complex(8) :: H(12,12)
+		complex(8) :: H(12,12),A(12,12),B(12,12)
+		integer :: i,j
+		write(*,*) ka,kb,kc!!!!!!!!!!!!!!!!!!!!
 		H = Hn(ka,kb,kc) + Hnn(ka,kb,kc)
+		A = Hn(0d0,0d0,0d0)
+		B = Hnn(0d0,0d0,0d0)
+		do i = 1,12
+			do j = 1,12
+				H(i,i) = H(i,i) + A(i,j) - B(i,j)
+			end do
+		end do
+
 	end function 
 	!-------------------------------------------------------
 
@@ -103,21 +150,21 @@ module Hamitonian
 		real(8),intent(out) :: k(knum,3)
 		
 		!---TH--------
-		k(1:knum/3,1) = vecn(0d0,0.5d0,knum/3)
-		k(1:knum/3,2) = vecn(0d0,0.5d0,knum/3)
-		k(1:knum/3,3) = vecn(0d0,-0.5d0,knum/3)
+		k(1:knum/3,1) = vecn(0d0,0.5d0*pi*2,knum/3)
+		k(1:knum/3,2) = vecn(0d0,0.5d0*pi*2,knum/3)
+		k(1:knum/3,3) = vecn(0d0,-0.5d0*pi*2,knum/3)
 		!-------------
 
 		!---HP-------
-		k(knum/3+1:2*knum/3,1) = vecn(0.5d0,0.25d0,knum/3)
-		k(knum/3+1:2*knum/3,2) = vecn(0.5d0,0.25d0,knum/3)
-		k(knum/3+1:2*knum/3,3) = vecn(-0.5d0,0.25d0,knum/3)
+		k(knum/3+1:2*knum/3,1) = vecn(0.5d0*pi*2,0.25d0*pi*2,knum/3)
+		k(knum/3+1:2*knum/3,2) = vecn(0.5d0*pi*2,0.25d0*pi*2,knum/3)
+		k(knum/3+1:2*knum/3,3) = vecn(-0.5d0*pi*2,0.25d0*pi*2,knum/3)
 		!------------
 
 		!---PT-------
-		k(2*knum/3+1:knum,1) = vecn(0.25d0,0d0,knum/3)
-		k(2*knum/3+1:knum,2) = vecn(0.25d0,0d0,knum/3)
-		k(2*knum/3+1:knum,3) = vecn(0.25d0,0d0,knum/3)
+		k(2*knum/3+1:knum,1) = vecn(0.25d0*pi*2,0d0,knum/3)
+		k(2*knum/3+1:knum,2) = vecn(0.25d0*pi*2,0d0,knum/3)
+		k(2*knum/3+1:knum,3) = vecn(0.25d0*pi*2,0d0,knum/3)
 		!------------
 	end subroutine
 	!--------------------------------------------------------
@@ -129,7 +176,7 @@ module Hamitonian
 		complex(8) :: H(12,12)
 
 		!----for zheev-----------
-		integer,parameter :: N = 12, Lwork = 100
+		integer,parameter :: N = 12, Lwork =24 
 		complex(8) :: Work(Lwork),VL(N,N),VR(N,N)
 		real(8) :: Rwork(3*N-2)
 		integer :: info
@@ -142,17 +189,18 @@ module Hamitonian
 		end if
 
 	end function
+	!------------------------------------------
 
 	subroutine sort(array,length)
 		integer,intent(in) :: length
 		real(8) :: array(length),b
-		integer :: i,j
+		integer :: m,j
 		logical :: exchange
 
 		exchange = .true.
 		
-		do i = length-1,0,-1
-			do j = 0,i
+		do m = length,1,-1
+			do j = 1,m-1
 				if (array(j)>array(j+1)) then
 					b = array(j)
 					array(j) = array(j+1)
@@ -174,9 +222,10 @@ program main
 	use Hamitonian
 	implicit none
 	integer,parameter :: knum=60
-	complex(8) :: E(12),motion(12,12)
+	real(8) :: E(12)
+	complex(8) :: motion(12,12),test(12,12)
 	real(8) :: k(knum,3)
-	integer :: i
+	integer :: i,m,j
 	call high_sym_line(k,knum)
 
 	open(unit=10,file='data/energyband.dat',status='unknown')
@@ -188,13 +237,24 @@ program main
 		motion(i,i) = -1
 	end do
 
-	do i = 1,knum
-		E = diag(matmul(motion,Ham(k(i,1),k(i,2),k(i,3))))
-		write(10,*) i,E(12),E(11),E(10),E(9),E(8),E(7),E(6),E(5),E(4),E(3),E(2),E(1)
-	end do 
+
+!	do i = 1,knum
+!		E = dble(diag(matmul(motion,Ham(k(i,1),k(i,2),k(i,3)))))
+!		write(*,*) E
+!		call sort(E,12)
+!		write(*,*) E
+!		stop !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!		write(10,*) i,E(12),E(11),E(10),E(9),E(8),E(7)!,E(6),E(5),E(4),E(3),E(2),E(1)
+!	end do 
 	
 !	write(*,*) diag(motion)
 !	E = diag(Ham(0.5d0,0.5d0,-0.5d0))
 !	write(*,*) E
-	write(*,*) Ham(0d0,0d0,-0d0)
+	test = Ham(2*pi/3,pi/2,pi/5)
+	open(unit=11,file='test.txt')
+	do i = 1,12
+		do j = 1,12
+			write(11,*) test(i,j)
+		end do 
+	end do
 end 
--- magnon/plot/plot.gp
+++ magnon/plot/plot.gp
@@ -3,8 +3,8 @@ set ylabel "E"
 
 plot "data/energyband.dat" u 1:2,"data/energyband.dat" u 1:3,\
 	     "data/energyband.dat" u 1:4,"data/energyband.dat" u 1:5,"data/energyband.dat" u 1:6,"data/energyband.dat" u 1:7,\
-	     "data/energyband.dat" u 1:8,"data/energyband.dat" u 1:9,"data/energyband.dat" u 1:10,"data/energyband.dat" u 1:11,\
-	     "data/energyband.dat" u 1:12,"data/energyband.dat" u 1:13
+# "data/energyband.dat" u 1:8,"data/energyband.dat" u 1:9,"data/energyband.dat" u 1:10,"data/energyband.dat" u 1:11,\
+#	     "data/energyband.dat" u 1:12,"data/energyband.dat" u 1:13
 
 set term pngcairo
 set output "plot/energyband.png"
