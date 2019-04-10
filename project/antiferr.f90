d8b9e05aa392902a4283e0c6c1e6fa654060fe40
Yupeng Wang <wangyp1@shanghaitech.edu.cn>
2019-04-10 20:45:04 +0800

    greenfun finish

--- project/antiferr.f90
+++ project/antiferr.f90
@@ -10,14 +10,14 @@ module constants
 	real(8),parameter :: hbar=1    !Plank constant
 	real(8),parameter :: N=1       !Number of atoms
 	real(8),parameter :: delta=0.01   !self energy
-	real(8),parameter :: z=4       !coordination number
+	real(8),parameter :: z=2       !coordination number
 	real(8),parameter :: E0=0      !energy of ground state
 	real(8),parameter :: J=1       !exchange constant 
 	real(8),parameter :: beta = 1  !temperature 
 	real(8),parameter :: pi=3.1415926        
 	real(8),parameter :: k = 2d0*pi/a
 	real(8),parameter :: Sz1 = 0.5,Sz2=-0.5 
-!	real(8) :: Sz1,Sz2
+	real(8) :: rSz1,rSz2
 	!-----------------------------------------------------------------------
 end module
 !-------------------------------------------------------------------
@@ -53,12 +53,13 @@ contains
 		!-------------------------------------------------------------
 		real(8) :: Jk,J0,kx,ky
 		complex(8) :: omega,green(2,2),c_delta
-		complex(8) :: det !detmination of eigenvector
 		integer :: m,n,l,k
 		complex(8) :: F(2,2)
+		common EIgenvalue
+
 		F(1,1:2) = (/2*Sz1,0d0/)
 		F(2,:) = (/0d0,2*Sz2/)
-		Jk = J*(2*cos(kx*a)+2*cos(ky*a))
+		Jk = J*(2*cos(kx*a))!+2*cos(ky*a))
 		J0 = z*J
 		P(1,1) = complex(-J0*Sz2,0)
 		P(1,2) = complex(Jk*Sz1,0)
@@ -68,6 +69,9 @@ contains
 		if (info .ne. 0) then
 			write(*,*) 'dgeev error'
 		end if
+!		write(*,*) Eigenvalue
+!		write(*,*) knum
+!		write(20,*) knum,dble(Eigenvalue(1)),dble(Eigenvalue(2))
 		p = Eigenvector!P is Eigenvector matrix
 		!compute the inverse of Eigenvectors
 		!compute green function
@@ -101,37 +105,44 @@ program main
 	use array
 	use calc
 	implicit none
-!	call calc_spin(-0.5d0,0.5d0,Sz1,Sz2)
-!	call iteration(0.5d0,-0.5d0,Sz1,Sz2)
-!	write(*,*) Sz1,Sz2
+	call calc_spin(-0.5d0,0.5d0,rSz1,rSz2)
+	call iteration(0.5d0,-0.5d0,rSz1,rSz2)
+	write(*,*) Sz1,Sz2
 	!------------------------------------------------------------------------------------------------
-	real(dp),parameter :: k_inter=0.01,o_inter = 0.01 !k_inter is interval of k, o_inter is interval of omega
-	real(dp),parameter :: o_max = 4d0, o_min=0d0  !o_max is max value of omega at omega axial
-	integer,parameter ::  k_num = int(k/k_inter)+1    !number of k point
-	integer,parameter ::  o_num = int((o_max-o_min)/o_inter)+1  !number of omega point
-	real(dp) :: k1(k_num),omega(o_num)
-	real(dp) :: spec1(k_num,o_num),spec2(k_num,o_num)
-	integer :: knum, onum
-	!---------------------------------------------------------------------------------------------------
-	!for spec
-	complex(8) :: green(2,2),c_omega(o_num)
+!	real(dp),parameter :: k_inter=0.01,o_inter = 0.01 !k_inter is interval of k, o_inter is interval of omega
+!	real(dp),parameter :: o_max = 3d0, o_min=0d0  !o_max is max value of omega at omega axial
+!	integer,parameter ::  k_num = int(k/k_inter)+1    !number of k point
+!	integer,parameter ::  o_num = int((o_max-o_min)/o_inter)+1  !number of omega point
+!	real(dp) :: k1(k_num),omega(o_num)
+!	real(dp) :: spec1(k_num,o_num),spec2(k_num,o_num)
+!	integer :: knum, onum
+!	!---------------------------------------------------------------------------------------------------
+!	!for spec
+!	complex(8) :: green(2,2),c_omega(o_num)
+!	
+!
+!
+!	complex(8) :: eigenvalue(2)
+!	common eigenvalue
+!	!------------------------------------------------------------------------------------------------------
+!	k1 = vec(-k/2d0,k/2d0,k_inter)
+!	omega = vec(o_min,o_max,o_inter)
+!	open(unit=20,file='data/dispersion.dat')
+!	open(unit=10,file='data/antiferr1.dat')
+!	open(unit=11,file='data/antiferr2.dat')
+!	do knum = 1,size(k1)
+!		do onum = 1,size(omega)
+!			c_omega(onum) = complex(omega(onum),0)
+!			green = -green_fun(k1(knum),0d0,c_omega(onum))
+!			spec1(knum,onum) = aimag(green(1,1))
+!			spec2(knum,onum) = aimag(green(2,2))
+!			write(10,*) k1(knum),omega(onum),spec1(knum,onum)
+!			write(11,*) k1(knum),omega(onum),spec2(knum,onum)
+!!			write(*,*) dble(eigenvalue(1)),dble(eigenvalue(2))
+!		end do
+!		write(20,*) k1(knum),dble(eigenvalue(1)),dble(eigenvalue(2))
+!	end do
 
-
-	!------------------------------------------------------------------------------------------------------
-	k1 = vec(-k,k,k_inter)
-	omega = vec(o_min,o_max,o_inter)
-	open(unit=10,file='data/antiferr1.dat')
-	open(unit=11,file='data/antiferr2.dat')
-	do knum = 1,size(k1)
-		do onum = 1,size(omega)
-			c_omega(onum) = complex(omega(onum),0)
-			green = -green_fun(k1(knum),0d0,c_omega(onum))
-			spec1(knum,onum) = aimag(green(1,1))
-			spec2(knum,onum) = aimag(green(2,2))
-			write(10,*) k1(knum),omega(onum),spec1(knum,onum)
-			write(11,*) k1(knum),omega(onum),spec2(knum,onum)
-		end do
-	end do
 
 !	use calc
 !	complex(8) :: test(2,2) 
@@ -250,7 +261,7 @@ subroutine calc_spin(Sz1_init,Sz2_init,Sz1_new,Sz2_new)
 	integer,parameter :: order = 2
 	complex(8) :: P(2,2),Phi(order)
 	real(8) :: J0 = z*J, Jk, Sz1_old, Sz2_old
-	integer,parameter :: k_num = 300
+	integer,parameter :: k_num = 30
 	integer :: knum,l,m
 	real(8) :: k1(k_num) 
 
@@ -270,16 +281,14 @@ subroutine calc_spin(Sz1_init,Sz2_init,Sz1_new,Sz2_new)
 
 	do  while(abs(Sz1_old-Sz1_new)>0.0001 .or. abs(Sz2_old-Sz2_new)>0.0001)
 
-		open(unit=20,file='aaa')
 	Sz1_old = Sz1_new
 	Sz2_old = Sz2_new
 
-	k1 = vecn(-k/2d0,k/2d0,k_num)
+	k1 = vecn(-k/2d0+0.1,k/2d0-0.1,k_num)
 	Phi = 0
 	do l=1,order
 		do m=1,order
 			do knum=1,k_num
-			Phi = 0	
 				Jk = J*(2*cos(k1(knum)*a)) !This line must be modified	when go to general
 				P(1,1) = -J0*Sz2_old
 				P(1,2) = Jk*Sz1_old
@@ -292,8 +301,9 @@ subroutine calc_spin(Sz1_init,Sz2_init,Sz1_new,Sz2_new)
 					write(*,*) 'zgeev error',info
 					stop
 				end if
-				write(*,*) Eigenvalue
-				write(20,*) k1(knum),real(eigenvalue(1)),real(eigenvalue(2)) 
+!				write(*,*) Eigenvalue
+!				stop
+	!			write(20,*) k1(knum),real(eigenvalue(1)),real(eigenvalue(2)) 
 		!		write(*,*) sqrt(J0**2-Jk**2)*Sz1
 !				write(*,*) Eigenvalue
 			!	if (abs(Eigenvalue(1)+Eigenvalue(2))>0.1) then
@@ -303,23 +313,27 @@ subroutine calc_spin(Sz1_init,Sz2_init,Sz1_new,Sz2_new)
 				inv = inverse(VR,2)
 		!		write(*,*) matmul(inv,VR)
 				Phi(l)=Phi(l) + 2*VR(l,m)*inv(m,l)/(exp(beta*Eigenvalue(m))-1) 
+!				write(*,*) k1(knum),Eigenvalue
 	!			write(*,*) VR(l,m)*inv(m,l)/(exp(beta*Eigenvalue(m))-1)
-			!	write(*,*)'Phi',Phi
+		!		write(*,*)'Phi',Phi(l)
 			end do
+		!	stop
 		end do
 		Phi(l) = Phi(l)/k_num
 !	write(*,*) 'phi',Phi
+!	stop
 !	write(*,*) Sz1_new,Sz2_new
 		
 	end do
 !	write(*,*) Phi
+	!stop
 	Sz1_new = 1/(2*(2*Phi(1)+1))
 	Sz2_new = 1/(2*(2*Phi(2)+1))
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	
-	stop!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	!stop!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -371,5 +385,5 @@ subroutine spin(Sz1_init,Sz2_init,Sz1_new,Sz2_new)
 
 	end do
 
-	write(*,*) Sz1_new,Sz2_new
+!	write(*,*) Sz1_new,Sz2_new
 end subroutine spin
